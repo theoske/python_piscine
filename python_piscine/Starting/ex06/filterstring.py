2e6e96b8d3cc21e6d76f28a5621a817b881eac5f
Theo Kempf <theokempf@protonmail.com>
2024-03-25 17:57:37 +0100

    ex06 updated

--- python_piscine/Starting/ex06/filterstring.py
+++ python_piscine/Starting/ex06/filterstring.py
@@ -7,29 +7,22 @@ def argTest(argv) -> bool:
         return False
 
     str_to_test = argv[1]
-    for c in str_to_test:
-        if c.isalpha() is False and c != " ":
-            return False
 
     try:
         int(argv[2])
     except ValueError:
         return False
 
-    return True
-
-
-def filterstring(string_to_filter: str):
-    return True if len(string_to_filter) >= int(argv[2]) else False
-
+    return all([False if c.isalpha() is False and c != " " else True for c in str_to_test]) is True
 
 def main(argv) -> int:
     if argTest(argv) is False:
         print(AssertionError.__name__ + ": Bad arguments.")
-        return (1)
+        return 1
     list_string_to_filter = argv[1].split(" ")
-    filtered = ft_filter(filterstring, list_string_to_filter)
+    filtered = ft_filter(lambda string_to_filter: len(string_to_filter) >= int(argv[2]), list_string_to_filter)
     print(filtered)
+    return 0
 
 
 if __name__ == "__main__":
--- python_piscine/Starting/ex06/ft_filter.py
+++ python_piscine/Starting/ex06/ft_filter.py
@@ -1,4 +1,4 @@
 def ft_filter(function: any, iterable) -> object:
     filtered_iterable = type(iterable)()
-    [filtered_iterable.append(item) for item in iterable if function(item)]
+    filtered_iterable = [item for item in iterable if function(item)]
     return filtered_iterable
