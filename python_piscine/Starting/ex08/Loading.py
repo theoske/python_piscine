62161161fc9764d6d6d812564f34ce7f5a5737c2
Theo Kempf <theokempf@protonmail.com>
2024-03-26 23:30:02 +0100

    ex08 finish

--- python_piscine/Starting/ex08/Loading.py
+++ python_piscine/Starting/ex08/Loading.py
@@ -1,8 +1,16 @@
-def ft_tqdm(lst: range) -> None:
-    i=0
-    while i <= lst[-1]:
-        last_in_range = lst[-1]
-        completion_proportion = (i*100) // last_in_range
-        print(str(completion_proportion) + "\%| [" + "=" * str(completion_proportion) + ">" + (" " * (100 - completion_proportion)) + "]| " + str(i) + str(lst))
-        i+=1
+from time import time
+from time import perf_counter
 
+def ft_tqdm(lst: range) -> None:
+    range_end = lst[-1] + 1
+    start_time = perf_counter() * 10**6 
+    yield
+    for n in lst:
+        elapsed_time = start_time - perf_counter()
+        frequency = (n / elapsed_time) * 10**6
+        eta = (range_end - n - 1) / frequency if frequency else 0
+        completion_proportion = ((n+1)*100) // range_end
+        progress_bar = f"{str(completion_proportion)}%| [{'=' * completion_proportion}>{' ' * (100 - completion_proportion)}]| {str(n+1)} / {str(range_end)} [eta: {eta:.1f}s, {frequency:.2f}it/s]"
+        print(progress_bar, end="\r")
+        start_time = perf_counter() * 10**6 
+        yield
--- python_piscine/Starting/ex08/tester.py
+++ python_piscine/Starting/ex08/tester.py
@@ -2,10 +2,9 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-
-for elem in ft_tqdm(range(333)):
+for elem in ft_tqdm(range(1000)):
     sleep(0.005)
 print()
-for elem in tqdm(range(333)):
-    sleep(0.005)
-print()
+# for elem in tqdm(range(333)):
+#     sleep(0.005)
+# print()
