51cccd2682ee26a307313c366eaf185c992f5245
Theo Kempf <theokempf@protonmail.com>
2024-03-27 14:39:34 +0100

    ex08

--- python_piscine/Starting/ex08/Loading.py
+++ python_piscine/Starting/ex08/Loading.py
@@ -1,7 +1,9 @@
-from time import time
 from time import perf_counter
+from os import get_terminal_size, system, name
 
 def ft_tqdm(lst: range):
+    width = get_terminal_size().columns
+    coeff = (width - 46)/width
     range_end = lst[-1] + 1
     start_time = perf_counter() * 10**6 
     yield
@@ -10,7 +12,10 @@ def ft_tqdm(lst: range):
         frequency = (n / elapsed_time) * 10**6
         eta = (range_end - n - 1) / frequency if frequency else 0
         completion_proportion = ((n+1)*100) // range_end
-        progress_bar = f"{str(completion_proportion)}%| [{'=' * completion_proportion}>{' ' * (100 - completion_proportion)}]| {str(n+1)} / {str(range_end)} [eta: {eta:.1f}s, {frequency:.2f}it/s]"
-        print(progress_bar, end="\r")
+        completion_length = int(width * coeff * completion_proportion / 100)
+        progress_bar = f"{str(completion_proportion)}%|{'█' * (completion_length)}{' ' * (int(width*coeff) - completion_length)}| {str(n+1)} / {str(range_end)} [eta: {eta:.1f}s, {frequency:.2f}it/s]"
+        if n>1:
+            system('cls' if name == 'nt' else 'clear')
+            print(progress_bar, end="\r")
         start_time = perf_counter() * 10**6 
         yield
