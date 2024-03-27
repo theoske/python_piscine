f318fd365500263dc72bbcda92c744a1e1537b02
Theo Kempf <theokempf@protonmail.com>
2024-03-27 15:13:59 +0100

    firstpackage

---
+++ python_piscine/Starting/ex09/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfirstpackage',  # Replace with your package name
+    version='4.2.0',   # Replace with your package version
+    author='theoske',  # Replace with your name
+    author_email='theokempf@protonmail.com',  # Replace with your email
+    description='just to try',
+    url='https://github.com/theoske/python_piscine/Starting/ex09/',  # Replace with your package URL
+    packages=find_packages(),  # Automatically find packages in the directory
+    classifiers=[
+        'Development Status :: 3',
+        'Intended Audience :: no one / everyone',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3',
+)
