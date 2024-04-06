07b31f65e3fab303f949d0fa8e90520410cf0d49
Theo Kempf <theokempf@protonmail.com>
2024-04-06 16:17:22 +0200

    init 03

---
+++ python_piscine/03oop/ex00/S1E9.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """blabla"""
+
+class Stark(Character):
+    """blabla"""
+
+
+Ned = Stark("Ned")
+print(Ned.__dict__)
+print(Ned.is_alive)
+Ned.die()
+print(Ned.is_alive)
+print(Ned.__doc__)
+print(Ned.__init__.__doc__)
+print(Ned.die.__doc__)
+print("---")
+Lyanna = Stark("Lyanna", False)
+print(Lyanna.__dict__)
